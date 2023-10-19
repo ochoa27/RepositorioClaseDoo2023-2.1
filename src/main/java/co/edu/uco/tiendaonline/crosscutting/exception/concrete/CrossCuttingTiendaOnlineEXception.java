22fd2e50b69ea4985efdb62ec8cf757f8ca37365
Andres Ochoa <andresochoaflorez0@gmail.com>
2023-10-18 22:59:00 -0500

    se actualiza crosscutting

--- src/main/java/co/edu/uco/tiendaonline/crosscutting/exception/concrete/CrossCuttingTiendaOnlineEXception.java
+++ src/main/java/co/edu/uco/tiendaonline/crosscutting/exception/concrete/CrossCuttingTiendaOnlineEXception.java
@@ -3,25 +3,25 @@ package co.edu.uco.tiendaonline.crosscutting.exception.concrete;
 import co.edu.uco.tiendaonline.crosscutting.exception.TiendaOnlineException;
 import co.edu.uco.tiendaonline.crosscutting.exception.enumerator.LugarException;
 
-public class CrossCuttingTiendaOnlineEXception extends TiendaOnlineException {
+public class CrosscuttingTiendaOnlineException extends TiendaOnlineException {
 	
 	private static final long serialVersionUID = -9177484194126685659L;
 	
-	protected CrossCuttingTiendaOnlineEXception( final Throwable exceptionRaiz,final  String mendajeUsuario,
+	protected CrosscuttingTiendaOnlineException( final Throwable exceptionRaiz,final  String mendajeUsuario,
 			final String mendajeTecnico) {
 		super(LugarException.CROSSCUTTING, exceptionRaiz, mendajeUsuario, mendajeTecnico);
 		
 	}
 	
 	public static final TiendaOnlineException crear (final String mensajeUsuario) {
-		return new CrossCuttingTiendaOnlineEXception(null, mensajeUsuario, mensajeUsuario);
+		return new CrosscuttingTiendaOnlineException(null, mensajeUsuario, mensajeUsuario);
 	}
 	
 	public static final TiendaOnlineException crear(final String mensajeUsuario,final String mensajeTecnico) {
-		return new CrossCuttingTiendaOnlineEXception(null, mensajeUsuario, mensajeTecnico);
+		return new CrosscuttingTiendaOnlineException(null, mensajeUsuario, mensajeTecnico);
 	}
 	public static final TiendaOnlineException crear(final Throwable  ExceptionRaiz,final String mensajeUsuario,final String mensajeTecnico) {
-		return new CrossCuttingTiendaOnlineEXception(ExceptionRaiz, mensajeUsuario, mensajeTecnico);
+		return new CrosscuttingTiendaOnlineException(ExceptionRaiz, mensajeUsuario, mensajeTecnico);
 	}
 
 }
